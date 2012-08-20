69848b882498f004049e6aa03ce88a00dce61e10
Mykhaylo Gavrylyuk <mykhaylo.gavrylyuk@gmail.com>
2012-08-21 00:51:22 +0300

    Upgraded Calculator demo and README

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 PageObject is an abstraction meaning a part of a web page maintating its specific logic over its components (DOM elements and other page objects).
 
-While creating rich web applications we always single out page blocks. We always refer to such blocks as objects when we need to repeat them here or there or on some other page. This is where the utility comes to hand.
+While creating rich web applications we used to single out page blocks. We refer to those blocks as objects when we need to repeat them somehow here or there. This is where the utility comes to hand.
 
 PageObject would help you if for such cases you used to render client-side templates and then revive their rendered results.
+
+Though template engine can be easily changed, PageObject out of the start works perfectly with both [underscore templates](http://underscorejs.com/#template) and [microtemplating engine](https://gist.github.com/1092301) by John Resig, determining without assistance if any of them is available.
 
 Please refer to demos and source code to understand how things work. More documentation and examples will follow.
 
--- demo/Calculator.js
+++ demo/Calculator.js
@@ -1,85 +1,82 @@
 (function ($, undefined) {
 
-var calculatorPageObjectOptions = {
-  template: "tmplCalculator",
-  context: {
-    caption: "Demo Calculator",
-  },
-  selectors: {
-    buttons: [ ':button', function (el) { return getButtonName(el); } ],
-    led: 'p'
-  }
-}
+  function Calculator(container) {
+    var history = [],
+      calc = $.PageObject({
+        template: $('#tmplCalculator').html(),
+        containerClass: 'calc',
+        context: {
+          caption: "Demo Calculator",
+        },
+        selectors: {
+          buttons: [ ':button', Calculator.getButtonName ],
+          led: 'p'
+        }
+      }),
+      $led = $(calc.DOM.led);
 
+    function remember() {
+      var current = $led.html();
+      if (current == history[history.length-1]) return;
+      history.push( $led.html() );
+    }
 
-function getButtonName(el) {
-  return $(el).attr('class').replace('calc-','');
-}
-
-function Calculator(container) {
-  var C = $.PageObject($.extend(true, {}, calculatorPageObjectOptions, { container: container }));
-  var history = [];
-  var $led = $(C.DOM.led);
-
-  function remember() {
-    var current = $led.html();
-    if (current == history[history.length-1]) return;
-    history.push( $led.html() );
-  }
-
-  for (var name in C.DOM.buttons)
-    $(C.DOM.buttons[name]).click(function () {
-      var name = getButtonName(this);
-      if (name == parseInt(name)) {
-        remember();
-        $led.html( $led.html() + ' ' + name );
-      }
-      else switch (name) {
-        case "cancel":
-          history = [];
-          $led.html('');
-          break;
-        case "undo":
-          $led.html(history.pop());
-          break;
-        case "plus":
+    for (var name in calc.DOM.buttons)
+      $(calc.DOM.buttons[name]).click(function () {
+        var name = Calculator.getButtonName(this);
+        if (name == parseInt(name)) {
           remember();
-          $led.html( $led.html() + ' + ' );
-          break;
-        case "minus":
-          remember();
-          $led.html( $led.html() + ' - ' );
-          break;
-        case "multiply":
-          remember();
-          $led.html( $led.html() + ' * ' );
-          break;
-        case "divide":
-          remember();
-          $led.html( $led.html() + ' / ' );
-          break;
-        case "equals":
-          var statement = $led.html().replace(/ /g, '');
-          var result;
-          try {
-            result = eval(statement);
-          } catch(e) {}
-          if (result !== undefined) {
+          $led.html( $led.html() + ' ' + name );
+        }
+        else switch (name) {
+          case "cancel":
+            history = [];
+            $led.html('');
+            break;
+          case "undo":
+            $led.html(history.pop());
+            break;
+          case "plus":
             remember();
-            $led.html( (result+"").replace(/./g, "$& ") );
-          }
-          break;
-        case "dot":
-          remember();
-          $led.html( $led.html() + ' .' );
-          break;
-      }
-    });
+            $led.html( $led.html() + ' + ' );
+            break;
+          case "minus":
+            remember();
+            $led.html( $led.html() + ' - ' );
+            break;
+          case "multiply":
+            remember();
+            $led.html( $led.html() + ' * ' );
+            break;
+          case "divide":
+            remember();
+            $led.html( $led.html() + ' / ' );
+            break;
+          case "equals":
+            var result,
+              statement = $led.html().replace(/ /g, '');
+            try {
+              result = eval(statement);
+            } catch(e) {}
+            if (result !== undefined) {
+              remember();
+              $led.html( (result+"").replace(/./g, "$& ") );
+            }
+            break;
+          case "dot":
+            remember();
+            $led.html( $led.html() + ' .' );
+            break;
+        }
+      });
 
-  return C;
-}
+    return calc;
+  }
 
+  Calculator.getButtonName = function (el) {
+    return $(el).attr('class').replace('calc-','');
+  }
 
-this.Calculator = Calculator;
+  window.Calculator = Calculator;
 
 })(jQuery);
